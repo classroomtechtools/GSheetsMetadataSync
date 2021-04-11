296bbc8a9c0fa3b69418c9c82bc229646f7418c0
Adam Morris <amorris@mistermorris.com>
2021-04-11 20:43:53 +0800

    added get, create, and update metadata

--- Exports.js
+++ Exports.js
@@ -4,3 +4,14 @@ function fromId(id, sheetKey) {
   return SheetsMetadataDoc.fromId(id, sheetKey);
 }
 
+function getMetadata(doc, metadataKey=null, metadataValue=null) {
+  return doc.getSpreadsheetMetadata({metadataKey, metadataValue});
+}
+
+function createMetadata(doc, metadataKey=null, metadataValue=null) {
+  return doc.createSpreadsheetMetadata({metadataKey, metadataValue});
+}
+
+function updateMetadata(doc, metadataId, metadataKey, metadataValue, visibility) {
+  return doc.updateSpreadsheetMetadata(metadataId, {metadataKey, metadataValue, visibility});
+}
